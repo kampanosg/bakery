bd6326ede33c2794c9c3e7abd80a75f0a905a4b6
George Kampanos <george.kampanos@nandos.co.uk>
2023-08-15 14:35:36 +0100

    added .gitignore

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	f, err := LoadBakefile()
 	if err != nil {
-		fmt.Printf("unable to load the Bakefile, %w", err)
+		fmt.Printf("unable to load the Bakefile, %v", err)
 		return
 	}
+	fmt.Printf("%v\n", f)
 }
