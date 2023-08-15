67d58babaa282a57de77cece9063b6038ed9e829
George Kampanos <george.kampanos@nandos.co.uk>
2023-08-15 18:15:11 +0100

    parse the bakefile

--- bakery.go
+++ bakery.go
@@ -12,4 +12,12 @@ func main() {
 	}
 	defer f.Close()
 	fmt.Printf("%v\n", f)
+
+	recipe, err := ParseBakefile()
+	if err != nil {
+		fmt.Printf("unable to parse the Bafile, %v", err)
+		return
+	}
+
+	fmt.Printf("%v\n", recipe)
 }
---
+++ models.go
@@ -0,0 +1,7 @@
+package main
+
+type (
+	Recipe struct {
+		Version string `yaml:"version"`
+	}
+)
