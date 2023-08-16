a046aa1912cc723d8ce39f24de4e43949354a8c3
George Kampanos <george.kampanos@nandos.co.uk>
2023-08-16 23:03:10 +0100

    basic skeleton for runner

---
+++ runner.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	HelpCommand = "help"
+)
+
+type (
+	CommandExecutor interface {
+		Run(cmd string) error
+	}
+
+	Runner struct {
+		executor CommandExecutor
+	}
+
+	DefaultExecutor struct{}
+)
+
+func NewDefaultRunner() *Runner {
+	return &Runner{
+		executor: &DefaultExecutor{},
+	}
+}
+
+func (r *Runner) RunCommand(recipe Recipe, cmd string) error {
+	switch cmd {
+	case HelpCommand:
+		fmt.Printf("help me\n")
+	default:
+		fmt.Printf("not found\n")
+	}
+	return nil
+}
+
+func (e *DefaultExecutor) Run(cmd string) error {
+	c := exec.Command("sh", "-c", cmd)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	if err := c.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
