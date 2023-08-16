1b30c8018fa9566eda7e0de4c52eb8084ae997dc
George Kampanos <george.kampanos@nandos.co.uk>
2023-08-16 18:02:25 +0100

    added tests for validity

---
+++ models_test.go
@@ -0,0 +1,57 @@
+package main
+
+import "testing"
+
+func TestBakery_Valid(t *testing.T) {
+	type fields struct {
+		Version string
+		Recipes []Recipe
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "error no recipes",
+			fields: fields{
+				Version: "1",
+			},
+			wantErr: true,
+		},
+		{
+			name: "success empty recipes",
+			fields: fields{
+				Version: "1",
+				Recipes: []Recipe{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "success",
+			fields: fields{
+				Version: "1",
+				Recipes: []Recipe{
+					{
+						Description: "a step to list the filesystem",
+						Default:     false,
+						Steps: []string{
+							"ls -al",
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Bakery{
+				Version: tt.fields.Version,
+				Recipes: tt.fields.Recipes,
+			}
+			if err := r.Valid(); (err != nil) != tt.wantErr {
+				t.Errorf("Bakery.Valid() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
