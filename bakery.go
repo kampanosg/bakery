5630a555daafaaae29cb56e2d75e6a2cb242a923
George Kampanos <george.kampanos@nandos.co.uk>
2023-08-17 14:18:38 +0100

    parse args ignoring flags

--- bakery.go
+++ bakery.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	f, err := LoadDefaultBakefile()
+	file := flag.String("file", "", "custom location for a Bakefile")
+	flag.Parse()
+
+	var f *os.File
+	var err error
+
+	if *file == "" {
+		f, err = LoadDefaultBakefile()
+	} else {
+		f, err = LoadBakefileFromLocation(*file)
+	}
+
 	if err != nil {
-		fmt.Printf("unable to load the Bakefile, %v", err)
+		fmt.Printf("unable to load the Bakefile, %v\n", err)
 		return
 	}
 	defer f.Close()
 
 	recipe, err := ParseBakefile(f)
 	if err != nil {
-		fmt.Printf("unable to parse the Bakefile, %v", err)
+		fmt.Printf("unable to parse the Bakefile, %v\n", err)
 		return
 	}
 
-	args := os.Args[1:]
+	args := make([]string, 0)
+	for i := 1; i <= len(os.Args[1:]); i++ {
+		arg := os.Args[i]
+		if strings.HasPrefix(arg, "--") {
+			i += 1
+			continue
+		}
+		args = append(args, arg)
+	}
 
 	runner := NewDefaultRunner()
 	if err := runner.RunCommand(recipe, args); err != nil {
-		fmt.Printf("run failed, %v", err)
+		fmt.Printf("run failed, %v\n", err)
 		return
 	}
 }
--- loader.go
+++ loader.go
@@ -15,11 +15,19 @@ var (
 
 func LoadDefaultBakefile() (*os.File, error) {
 	for _, b := range DefaultBakefiles {
-		file, err := os.Open(b)
+		file, err := open(b)
 		if err != nil {
 			continue
 		}
 		return file, nil
 	}
 	return nil, fmt.Errorf("unable to find Bakefile")
+}
+
+func LoadBakefileFromLocation(path string) (*os.File, error) {
+	return open(path)
+}
+
+func open(file string) (*os.File, error) {
+	return os.Open(file)
 }
